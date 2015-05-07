bd0c578aecdc5cc0f60a58680479c2f6c8799212
Andrew Kim <andykim5@uw.edu>
2015-05-07 15:55:46 -0700

    php

---
+++ Structure/footer.php
@@ -0,0 +1,9 @@
+<html>
+	<div class="container">
+		<div id="footer">
+			<div id="footer1">
+				This is my footer.
+			</div>
+		</div>
+	</div>
+</html>
---
+++ Structure/sidebar.php
@@ -0,0 +1,13 @@
+<html>
+<link rel=stylesheet type="text/css" href="http://andykim5.github.io/CSS/version1.css">
+<link rel=stylesheet type=""text/css" href="http://fonts.googleapis.com/css?family=Montserrat:400,700">
+	<div class="container">
+		<div id="sidebar">
+			<div id="toc">
+				<a href="http://andykim5.github.io">Home</a>
+				<a href="http://andykim5.github.io/Blog/BlogHome.html">Blog</a>
+				Travel
+			</div>
+		</div>
+	</div>
+</html>
